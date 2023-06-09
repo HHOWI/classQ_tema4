fa755c60e65e8ddfaac071bfcbab533d83572b38
johnmayer <130232731+mayerjogn@users.noreply.github.com>
2023-06-09 17:42:08 +0900

    aaa

--- team4/src/team4/model/Admin.java
+++ team4/src/team4/model/Admin.java
@@ -1,10 +1,17 @@
 package team4.model;
 
 public class Admin {
+
 	
-private String ban;//블랙 
-private String QAanswer;//QA답변
-private String managesite;//사이트 전체적인 관리
-private String manageuser;//유저 관리
-private String manageboard;//게시판 관리
+	private String logo;// 사이트 로고?
+	private String id;// 관리자 id
+	private String pw;// 관리자 pw
+	private String nickname;// 관리자 닉네임
+	private String addManager;// 새로운 관리자 추가
+	private String ban;// 블랙
+	private String QAanswer;// QA답변
+	private String managesite;// 사이트 전체적인 관리
+	private String manageuser;// 유저 관리
+	private String manageboard;// 게시판 관리
+
 }
