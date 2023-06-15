65744aca2c08c6c4ec9ce45691f8bd8a5859a751
HHOWI <minsu705@gmail.com>
2023-06-15 14:44:03 +0900

    ㅎㅇ

---
+++ team4/src/team4/impl/CommentImpl.java
@@ -0,0 +1,15 @@
+package team4.impl;
+
+import team4.model.Comment;
+
+public interface CommentImpl {
+
+	public void addComment(String id, String password, Comment comment);
+
+	public Comment viewComment(int index);
+
+	public void updateComment(int index, Comment comment);
+
+	public void deleteComment(int index);
+	
+}
--- team4/src/team4/model/Comment.java
+++ team4/src/team4/model/Comment.java
@@ -2,4 +2,11 @@ package team4.model;
 
 public class Comment {
 
+	private String id;
+	private String comment;
+	private String writingTime;
+	private String like;
+	
+	private User user;
+	
 }
