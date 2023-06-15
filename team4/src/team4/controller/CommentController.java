80fb4763a14de27dc8e60523ac2b5f040fb74bc3
Jykim048 <jylove0408@naver.com>
2023-06-15 16:22:46 +0900

    Update CommentController.java

--- team4/src/team4/controller/CommentController.java
+++ team4/src/team4/controller/CommentController.java
@@ -11,23 +11,26 @@ public class CommentController {
 	// 댓글 작성
 	public void addComment(String id, String password, Comment comment) {
 
-//		if(uac.login(id, password)) {          // UserAdminController 로그인
-//			comments.add(comment);
-//		}
+		if(uac.login(id, password)) {          // UserAdminController 로그인
+			comments.add(comment);
+		}
 	}
 
-	// 댓글 1개 보기
+	// 댓글 보기
 	public Comment viewComment(int index) {
 		return comments.get(index);
 	}
 
 
-	// 댓글 수정 
+	// 댓글 수정 (로그인한 회원과 댓글 작성자가 일치)
 	public void updateComment(int index, Comment comment) {
+		
+	
+		
 		comments.set(index, comment);
 	}
 
-	// 댓글 삭제
+	// 댓글 삭제 (로그인한 회원과 댓글 작성자가 일치)
     public void deleteComment(int index) {
     	comments.remove(index);
     }
