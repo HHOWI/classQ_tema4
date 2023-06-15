8e182054719e9d0af0eaa8a27cab0ba2ae780c34
HHOWI <minsu705@gmail.com>
2023-06-15 16:26:41 +0900

    ㅇㅁㄴ

--- team4/src/team4/model/Comment.java
+++ team4/src/team4/model/Comment.java
@@ -5,7 +5,7 @@ public class Comment {
 	private String id;
 	private String comment;
 	private String writingTime;
-	private String like;
+	private String like;			// 좋아요만 있고 싫어요는 없는게 컨셉
 	
 	private User user;
 	
