4936123557d960d83d20d0354b3cc304d5ac750a
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-06-15 15:52:30 +0900

    홍수민

--- team4/src/team4/controller/CommentController.java
+++ team4/src/team4/controller/CommentController.java
@@ -1,5 +1,40 @@
 package team4.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import team4.model.Comment;
+
 public class CommentController {
 
+	List<Comment> comments = new ArrayList<>();
+	UserAdminController uac = new UserAdminController();
+
+	// C : 댓글 작성 --> 로그인된 회원만 가능!
+	public void addComment(String id, String password, Comment comment, String writingTime ) {
+		
+	}
+
+	// R : 댓글 1개 보기
+	public Comment viewComment(int index) {
+		return comments.get(index);
+	}
+
+	// U : 댓글 수정 --> 로그인한 회원과 이 댓글을 작성한 작성자가 일치
+	public void updateComment(int index, Comment comment) {
+		comments.set(index, comment);
+	}
+
+	// D : 댓글 삭제 --> 로그인한 회원과 이 댓글을 작성한 작성자가 일치
+	public void deleteComment(int index) {
+		comments.remove(index);
+	}
+	
+	public void replyComment(String id, String password, Comment comment) { // 답글
+		
+	}
+	
+	public void likeComment(int index) { // 좋아요
+		
+	}
 }
--- team4/src/team4/model/Comment.java
+++ team4/src/team4/model/Comment.java
@@ -8,5 +8,47 @@ public class Comment {
 	private String like;
 	
 	private User user;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getWritingTime() {
+		return writingTime;
+	}
+
+	public void setWritingTime(String writingTime) {
+		this.writingTime = writingTime;
+	}
+
+	public String getLike() {
+		return like;
+	}
+
+	public void setLike(String like) {
+		this.like = like;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
 	
 }
