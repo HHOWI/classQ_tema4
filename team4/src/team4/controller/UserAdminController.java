c1706e86f65cce272e9c418d699d69365b3448b4
yejin1219 <130232731+mayerjogn@users.noreply.github.com>
2023-06-15 16:25:30 +0900

    UserAdminController

--- team4/src/team4/controller/UserAdminController.java
+++ team4/src/team4/controller/UserAdminController.java
@@ -1,6 +1,61 @@
 package team4.controller;
 
+import java.util.HashMap;
+
+import team4.model.Admin;
+import team4.model.User;
+
 public class UserAdminController {
 
-//	das
+	Admin admin = null;
+	private HashMap<String,User> hMap = new HashMap<>();
+	public void addManager() {// 관리자 추가
+
+	}
+		
+	
+	public boolean login(String id, String password) {// 관리자로그인
+		if(admin != null && admin.getId()
+	.equals(id)&&admin.getPassword().equals(password)) {
+		return true;	
+		}
+			return false;
+	}
+	public void setnickName(String nickName) {// 닉네임설정
+		admin.setNickName(nickName);
+	}
+		
+	// 유저 관리
+	public void  userViewProfile(){
+		
+	}
+	public void userUpdateProfile() {
+		
+	}
+	public boolean userDeleteProfile(String id) {//  계정삭제
+		
+		if(hMap.containsKey(id)) {
+			hMap.remove(id);
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public void blackUser(String blackUser) {// 유저 밴
+		
+		
+	}
+	//여기까지 유저관리
+	
+	public void answerQA() {// QA답변
+		
+	}
+	
+	
+	
+		
+
+		
 }
+	
--- team4/src/team4/model/Admin.java
+++ team4/src/team4/model/Admin.java
@@ -6,5 +6,43 @@ public class Admin {
 	private String id;// 관리자 id
 	private String password;// 관리자 pw
 	private String nickName;// 관리자 닉네임
+	public Admin() {
+		super();
+	}
+	public Admin(String logo, String id, String password, String nickName) {
+		super();
+		this.logo = logo;
+		this.id = id;
+		this.password = password;
+		this.nickName = nickName;
+	}
+	public String getLogo() {
+		return logo;
+	}
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	@Override
+	public String toString() {
+		return "Admin [logo=" + logo + ", id=" + id + ", password=" + password + ", nickName=" + nickName + "]";
+	}
 	
 }
