125e8f656e6e4c3902d73da7f01e1d1649e668bb
HHOWI <minsu705@gmail.com>
2023-06-16 11:28:38 +0900

    dsa

--- team4/src/team4/model/Board.java
+++ team4/src/team4/model/Board.java
@@ -2,19 +2,14 @@ package team4.model;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
-public class Board {
+public class Board extends Post {
 
-	private String boardName; 		 // 게시판 이름
-	private int postPage;            // 게시글 페이지 수
-	private List visitBoardList;     // 마지막 방문 게시판목록
- 
-	
-	
-
-	
-
-}
-
+	private String boardName; // 게시판 이름
+	private int postPage; // 게시글 페이지 수
+	private List visitBoardList; // 마지막 방문 게시판목록
+	private String serch; 		// 검색란
+	private String serchSelect; // 검색 선택란 (말머리, 작성자, 게시글제목 ...)
 
 
