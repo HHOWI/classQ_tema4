db8bf84a6ef1943ec03b93d4b55d5dc746698fb2
HHOWI <minsu705@gmail.com>
2023-06-16 15:11:07 +0900

    ㅇㅁㄴㅇㅁㄴ

--- team4/src/team4/controller/UserAdminController.java
+++ team4/src/team4/controller/UserAdminController.java
@@ -12,122 +12,160 @@ import team4.model.Comment;
 import team4.model.User;
 
 public class UserAdminController {
-	
+
 	User user = new User();
 	boolean admin = user.isAdmin();
 	List<Comment> comments = new ArrayList<>();
-	private HashMap<String,User> hMap = new HashMap<>();
+	private HashMap<String, User> hMap = new HashMap<>();
 	private ArrayList<User> list = new ArrayList<>();
-	
+
 	public void signUp(User user) {
-		
+
 		list.add(user);
 	}
 
+	public void login(String id, String password) {
 
+		boolean successLogin = false;
 
+		for (User user : list) {
 
-	public boolean addManager(String id, User a) {
-		if(hMap.containsKey(id)) {
-			hMap.put(id, a);		
-			return true;
+			if (user.getId().equals(id) && user.getPassword().equals(password)) {
+				successLogin = true;
+				break;
+			}
+
 		}
-		return false;
-	}
-	public void addManager(User user ) {
-		if(user.isAdmin()) {
-			user.setId(user.getId());
-			user.setPassword(user.getPassword());
-			user.setAdmin(true);
+		if (successLogin) {
+			System.out.println("로그인 성공!");
+		} else {
+			System.out.println("아이디 또는 패스워드가 일치하지 않습니다.");
 		}
 
+	}
+
+	public void logout() {
 
 	}
-		
-	
-	public void login(User user) {// 관리자로그인
-		if(user.isAdmin()) {
-			user.getId();
-			user.getPassword();
-		}
-	}
-	
-	public boolean setnickName(String nickName) {// 닉네임설정
-		if(hMap.containsKey(nickName)) {
-			hMap.put(nickName, null);
-		
-		return true;
-	}
-	return false;
-}
 
-		
+	public void addManager(String id, String password) {
+
+		user.setId(id);
+		user.setPassword(password);
+		user.setAdmin(true);
+
+	}
+
+	public void setnickName(String nickName) {// 닉네임설정, 로그인한 상태에서 수정할 수 있도록 구현해야함(구현방법을 모르겠어요 ㅠㅠ)
+
+		user.setNickname(nickName);
+
+	}
+
 	// 유저 관리
-	public void  userViewProfile(User viewuser){// 유저 계정 보기
-		if(user.isAdmin()) {
-			System.out.print(Arrays.toString(list));
+	public List<User> userViewProfile(String userId) {// 유저 계정 정보 보기
+
+		List<User> matchingUsers = new ArrayList<>();
+
+		for (User user : list) {
+			if (user.getId().equals(userId)) {
+				matchingUsers.add(user);
+			}
 		}
-		
+		return matchingUsers;
+
 	}
-	public void userUpdateProfile(User updateuser) {// 유저 계정 업데이트
-		
-		
+
+	public void userUpdateProfile(User user, String fieldName, String updatedValue) {// 유저 계정 정보 수정
+
+		switch (fieldName) {
+		case "name":
+			user.setName(updatedValue);
+			break;
+		case "password":
+			user.setPassword(updatedValue);
+			break;
+		case "age":
+			try {
+				int age = Integer.parseInt(updatedValue);
+				user.setAge(age);
+			} catch (NumberFormatException e) {
+				System.out.println("유효한 나이 값을 입력하세요.");
+			}
+			break;
+		case "gender":
+			user.setGender(updatedValue);
+			break;
+		case "city":
+			user.setName(updatedValue);
+			break;
+		case "phoen":
+			user.setPhone(updatedValue);
+			break;
+		case "email":
+			user.setEmail(updatedValue);
+			break;
+		case "statusMessge":
+			user.setStatusMessge(updatedValue);
+			break;
+		case "nickname":
+			user.setNickname(updatedValue);
+			break;
+		case "birtday":
+			user.setBirthday(updatedValue);
+			break;
+		case "mbti":
+			user.setMbti(updatedValue);
+			break;
+		case "bloodType":
+			user.setBloodType(updatedValue);
+			break;
+		case "lover":
+			user.setLover(false);
+			break; // 추후 수정
+
+		}
+
 	}
-	public boolean userDeleteProfile(String id) {//  계정삭제
-		
-		if(hMap.containsKey(id)) {
+
+	public boolean userDeleteProfile(String id) {// 계정삭제
+
+		if (hMap.containsKey(id)) {
 			hMap.remove(id);
 			return true;
 		}
 		return false;
-		
-	}
-			
 
-	public<blackUser> void black(/*String blackUser*/) {// 유저 밴 며칠이나 몇개월로 설정하고 싶은데 못해서 일단 걍때려박았음
+	}
+
+	public <blackUser> void black(/* String blackUser */) {// 유저 밴 며칠이나 몇개월로 설정하고 싶은데 못해서 일단 걍때려박았음
 		Scanner sc = new Scanner(System.in);
-	
+
 		System.out.print("블랙시킬 기간을 입력하세요 : ");
 
-		
 		System.out.print("년 : ");
 		int year = Integer.parseInt(sc.nextLine());
-		
+
 		System.out.print("월 : ");
 		int month = Integer.parseInt(sc.nextLine());
-		
+
 		System.out.print("일 : ");
 		int day = Integer.parseInt(sc.nextLine());
-		
-		LocalDate nowDate = LocalDate.now();		
+
+		LocalDate nowDate = LocalDate.now();
 		LocalDate banDate = LocalDate.of(year, month, day);
 		long dDay = ChronoUnit.DAYS.between(nowDate, banDate);
-		dDay = nowDate.until(banDate,ChronoUnit.DAYS );
-		if(dDay==0) {
-			System.out.println("오늘 밴이 해제됩니다.");			
-		}else if(dDay>0) {
-			System.out.println("밴 해제일로부터"+dDay+"남았습니다");
-		}else {
-			
+		dDay = nowDate.until(banDate, ChronoUnit.DAYS);
+		if (dDay == 0) {
+			System.out.println("오늘 밴이 해제됩니다.");
+		} else if (dDay > 0) {
+			System.out.println("밴 해제일로부터" + dDay + "남았습니다");
+		} else {
+
 		}
-		
+
 	}
-	//여기까지 유저관리
-	
-	public void answerQA(Comment comment) {// QA답변
-		
-		
-		
-		if(user.getId().equals(user.getId()) ) {
-			comments.add(comment);
-		}else {
-			System.out.println("Admin 전용입니다.");
-		}
-		
-		
-		
-	}
-	
-	
+	// 여기까지 유저관리
+
+
 }
-	
